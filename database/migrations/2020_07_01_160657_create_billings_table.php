ed37d96caa73dec5832f5ebcfc4e5c7dce28d0d4
naufalandika <andikaa.naufal@gmail.com>
2020-07-01 23:13:54 +0700

    Create billings migration

---
+++ database/migrations/2020_07_01_160657_create_billings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->integer('price');
+            $table->integer('discount');
+            $table->date('pay_before');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billings');
+    }
+}
