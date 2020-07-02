3a2ad92967b707cd1ecaa1de3b0d5223ce848a0b
naufalandika <andikaa.naufal@gmail.com>
2020-07-02 17:18:19 +0700

    Send email when billing created

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -4,28 +4,55 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\BillingRequest;
 use App\Billing;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\BillingCreated;
 
 class BillingController extends Controller
 {
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\BillingRequest  $request
+     * @param  \App\Http\Requests\BillingRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(BillingRequest $request)
     {
-        $data = [
+        $data = $this->fetchStore($request);
+
+        $billing = Billing::create($data);
+        $billing->generateNumber();
+
+        $this->sendBilling($billing);
+
+        return rest_api($billing, 201);
+    }
+
+    /**
+     * Fetch request data.
+     * 
+     * @param BillingRequest $request
+     * @return array
+     */
+    private function fetchStore($request)
+    {
+        return [
             'product_name'  => $request->product_name,
             'price'         => $request->price,
             'discount'      => $request->discount,
             'pay_before'    => $request->pay_before,
             'email'         => $request->email
         ];
+    }
 
-        $billing = Billing::create($data);
-        $billing->generateNumber();
-
-        return rest_api($billing, 201);
+    /**
+     * Send billing to email.
+     * 
+     * @param Billing $billing
+     * @return void
+     */
+    private function sendBilling(Billing $billing)
+    {
+        Mail::to($billing->email)
+            ->queue(new BillingCreated($billing));
     }
 }
